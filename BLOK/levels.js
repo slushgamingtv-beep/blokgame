d68357f9ea690c6c134751445db0bdfe1288f6f8
slushgamingtv-beep <slushgamingtv@gmail.com>
2026-02-27 00:03:47 +0100

    Delete BLOK directory

--- BLOK/levels.js
+++
@@ -1,9 +0,0 @@
-function loadLevel(level) {
-    // ensure the global currentLevel matches the requested level
-    if (typeof currentLevel !== 'undefined') currentLevel = level;
-    resetArrays();
-    spawnPlayers();
-    if (level === 1) loadLevel1();
-    else if (level === 2) loadLevel2();
-    else if (level === 3) loadLevel3();
-}
