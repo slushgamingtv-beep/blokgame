6b9216e476f785eed8c3badb2c27313285883b0f
slushgamingtv-beep <slushgamingtv@gmail.com>
2026-02-27 11:46:19 +0100

    Delete BLOK directory

--- BLOK/level3.js
+++
@@ -1,7 +0,0 @@
-function loadLevel3() {
-    // Simple empty level: only a floor
-    const groundY = canvas.height - 60;
-    // Make a long floor platform
-    platforms.push(new Platform(0, groundY, 2000, 60));
-    // No other entities (no lasers, buttons, keys, doors)
-}
